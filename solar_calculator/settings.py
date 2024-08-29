d6946411314eb96f740f8955dcf0a3bfbe38e3a2
rxequiel <delahoz27montes@gmail.com>
2024-08-29 15:34:35 -0500

    Configura la conexión a PostgreSQL, se crean apps users y solar_systems,se configura JWT y completa migraciones

--- solar_calculator/settings.py
+++ solar_calculator/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'solar_systems',
 ]
 
 MIDDLEWARE = [
@@ -82,6 +85,20 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432',
     }
+}
+
+#autenticacion 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+SIMPLE_JWT={
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=40),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+
 }
 
 
